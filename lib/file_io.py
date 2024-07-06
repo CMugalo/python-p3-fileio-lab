b7003b9b512a77dab78d60331b5496343c764f13
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-06 18:56:10 +0300

    practiced using file IO

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode = "w", encoding = "utf-8") as file_content:
+        file_content.write("This is a test content.")
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode = "a", encoding = "utf-8") as append_content:
+        append_content.write("\nAppended content.")
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode = "r", encoding = "utf-8") as read_content:
+        return read_content.read()
+
+
